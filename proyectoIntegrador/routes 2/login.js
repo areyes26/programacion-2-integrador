765d1df4b56a07566f52587ba32fdc6ae9623ba2
Francisco <ffranciscocasas@gmail.com>
2020-05-17 15:44:15 -0300

    Copy paste de lo que habia hecho fuera
    las carpetas routes 2 y views 2 tienen que ser modificadas, son las mismas con las que habia creado fuera de esto.

---
+++ proyectoIntegrador/routes 2/login.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var router = express.Router();
+var moduleControllers = require('../controllers/moduleControllers');
+
+/* GET home page. */
+
+router.get('/', moduleControllers.lista);
+router.get('/add', moduleControllers.add);
+router.post('/add', moduleControllers.save);
+router.post('/delete/:id', moduleControllers.delete);
+
+router.get('/edit/:id', moduleControllers.edit);
+router.post('/edit/:id', moduleControllers.actualizar);
+
+
+module.exports = router;
