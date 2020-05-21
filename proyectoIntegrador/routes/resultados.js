ffa779f51cf4fd5e8747e9695a88fc3b1278cc4b
nico <nicocappone18@gmail.com>
2020-05-21 20:01:24 -0300

    errores form

--- proyectoIntegrador/routes/resultados.js
+++ proyectoIntegrador/routes/resultados.js
@@ -5,7 +5,59 @@ let Op = db.Sequelize.Op;
 
 const controller = require('../controllers/resultados_controller');
 
+function validarformulario(formulario) {
+  let errores = [];
 
+  function validarPelicula(pelicula) {
+    let errores = [];
+
+    if (pelicula.title == "") {
+        errores.push("Por favor dejame el titulo completo, no vacío che")
+    } else if (pelicula.title.length < 3) {
+        errores.push("Che amigo, al menos 3 caracteres")
+    }
+
+    if (isNaN(pelicula.length)) {
+        errores.push("Che amigo, la duracion tiene que ser un numero")
+    }
+
+    return errores;
+}
+
+
+  if (isNaN(formulario.password)) {
+      errores.push("Che amigo, la contraseña tiene que ser un numero")
+  }
+
+  return errores;
+}
+
+
+router.post('/errores', function(req, res) {
+  let formulario = {
+    fullname: req.body.fullname,
+    username: req.body.username,
+    email: req.body.email,
+    password: req.body.password,
+   
+}
+
+let errores = validarformulario(formulario)
+
+if (errores.length > 0) {
+  // Hubieron errores => Volver a mostrar la pagina con el form y los errores
+  
+      res.render("#modal-usuario", {
+         
+          errores: errores
+      })
+  
+} else {
+  // No hubieron errores, todo bien :)
+  res.send()
+  // Guardarla en base de datos....
+}
+})
 
 router.get('/', function(req, res, next) {
     
