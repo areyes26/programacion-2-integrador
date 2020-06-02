bc2ca09e1f7d6fff72c0f3289caab2e98da088db
Francisco <ffranciscocasas@gmail.com>
2020-06-02 20:34:49 -0300

    comments

--- proyectoIntegrador/comments.js
+++
@@ -1,7 +0,0 @@
-// A continuacion me hare un resumen para cambiarle los colores a los comentarios
-
-//? Para el color azul
-//* Para el color verde
-//! Para el color rojo
-//TODO para el color naranja
-//// Para el comentario tachado
