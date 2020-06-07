707607a5c9c53ff4866f96231a82e40fa78046ff
nico <nicocappone18@gmail.com>
2020-06-07 20:18:20 -0300

    terminado puts

--- proyectoIntegrador/controllers/resultados_controller.js
+++ proyectoIntegrador/controllers/resultados_controller.js
@@ -1,5 +1,90 @@
+let db = require('../database/models');
+let Op = db.Sequelize.Op;
+let bycrypt = require('bcryptjs');
+
+
+
 let controlador = {
-	/* Aca se escribe lo que se ejecuta*/
+
+buscador: function (req, res, next) {
+	db.User.findAll({
+		where: [{ email: { [Op.like]: '%' + req.query.busqueda + '%' } }]
+	}).then((User) => {
+		if (User == 0) {
+			db.User.findAll({
+				where: [{ username: { [Op.like]: '%' + req.query.busqueda + '%' } }]
+			}).then((name) => {
+				if (name == 0) {
+					res.render('resultados', { title: 'Express' });
+				} else {
+					res.render('buscador', {
+						name: name
+					});
+				}
+			});
+		} else {
+			res.render('buscador2', {
+				User: User
+			});
+		}
+	});
+},
+
+
+	
+	errores: function (req, res) {
+
+		function validarformulario(formulario) {
+			let errores = [];
+		
+			if (formulario.fullname == '') {
+				errores.push('Por favor dejame el titulo completo, no vacío che');
+			} else if (formulario.username.length < 3) {
+				errores.push('Che amigo, al menos 3 caracteres');
+			} else if (formulario.password.length < 5) {
+				errores.push('La contraseña es muy debil tiene que ser mas larga');
+			} else if (formulario.genero_id == '') {
+				errores.push('Falta dejar el genero favorito');
+			}
+		
+			return errores;
+		}
+
+
+
+		let formulario = {
+			fullname: req.body.fullname,
+			username: req.body.username,
+			email: req.body.email,
+			password: bycrypt.hashSync(req.body.password, 10),
+			genero_id: req.body.genero_id
+		};
+	
+		let errores = validarformulario(formulario);
+	
+		if (errores.length > 0) {
+			// Hubieron errores => Volver a mostrar la pagina con el form y los errores
+			req.session.erroresregistracion = errores;
+			res.redirect('back');
+		} else {
+			let user = {
+				fullname: req.body.fullname,
+				username: req.body.username,
+				email: req.body.email,
+				password: bycrypt.hashSync(req.body.password, 10),
+				birthday: req.body.birthday,
+				genero_id: req.body.genero_id,
+				serie_favorita: req.body.serie_favorita
+			};
+	
+			db.User.create(user).then(() => {
+				res.redirect('/');
+			});
+	
+			// Guardarla en base de datos....
+		
+		}
+	}
 };
 
 module.exports = controlador;
--- proyectoIntegrador/routes/resultados.js
+++ proyectoIntegrador/routes/resultados.js
@@ -1,81 +1,12 @@
 var express = require('express');
 var router = express.Router();
-let db = require('../database/models');
-let Op = db.Sequelize.Op;
-let bycrypt = require('bcryptjs');
-const controller = require('../controllers/resultados_controller');
 
-function validarformulario(formulario) {
-	let errores = [];
 
-	if (formulario.fullname == '') {
-		errores.push('Por favor dejame el titulo completo, no vacío che');
-	} else if (formulario.username.length < 3) {
-		errores.push('Che amigo, al menos 3 caracteres');
-	} else if (formulario.password.length < 5) {
-		errores.push('La contraseña es muy debil tiene que ser mas larga');
-	} else if (formulario.genero_id == '') {
-		errores.push('Falta dejar el genero favorito');
-	}
+let controller = require('../controllers/resultados_controller');
 
-	return errores;
-}
 
-router.post('/errores', function (req, res) {
-	let formulario = {
-		fullname: req.body.fullname,
-		username: req.body.username,
-		email: req.body.email,
-		password: bycrypt.hashSync(req.body.password, 10),
-		genero_id: req.body.genero_id
-	};
 
-	let errores = validarformulario(formulario);
+router.post('/errores', controller.errores);
 
-	if (errores.length > 0) {
-		// Hubieron errores => Volver a mostrar la pagina con el form y los errores
-		req.session.erroresregistracion = errores;
-		res.redirect('back');
-	} else {
-		let user = {
-			fullname: req.body.fullname,
-			username: req.body.username,
-			email: req.body.email,
-			password: bycrypt.hashSync(req.body.password, 10),
-			birthday: req.body.birthday,
-			genero_id: req.body.genero_id,
-			serie_favorita: req.body.serie_favorita
-		};
-
-		db.User.create(user).then(() => {
-			res.redirect('/');
-		});
-
-		// Guardarla en base de datos....
-	}
-});
-
-router.get('/', function (req, res, next) {
-	db.User.findAll({
-		where: [{ email: { [Op.like]: '%' + req.query.busqueda + '%' } }]
-	}).then((User) => {
-		if (User == 0) {
-			db.User.findAll({
-				where: [{ username: { [Op.like]: '%' + req.query.busqueda + '%' } }]
-			}).then((name) => {
-				if (name == 0) {
-					res.render('resultados', { title: 'Express' });
-				} else {
-					res.render('buscador', {
-						name: name
-					});
-				}
-			});
-		} else {
-			res.render('buscador2', {
-				User: User
-			});
-		}
-	});
-});
+router.get('/', controller.buscador);
 module.exports = router;
